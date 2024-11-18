70e20f4fd2497ddd9fd97a73dc174a26699d384c
dillanfdr <nurfadillahdwirahma@gmail.com>
2024-11-19 00:11:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar } from "./components/navbar/navbar";
 import { Footer } from "./components/footer/footer";
 
 import { Homepage } from "./components/homepage/homepage";
-import { Inspiration } from "./inspriration/inspiration";
+import { Inspiration } from "./components/inspiration/inspiration";
 import { Article2 } from "./components/article2/article2";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -11,13 +11,13 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
-
+			
 			<Routes>
 				<Route path="/" element={<Homepage />} />
 				<Route path="article2" element={<Article2 />} />
 				<Route path="inspiration" element={<Inspiration />} />
 			</Routes>
-
+			
 			<Footer />
 		</BrowserRouter>
 	);
