8d1f296437edaccde0302766f823669d0403d626
javasantika@gmail.com <javasantika@gmail.com>
2024-11-19 20:34:00 +0700

    property

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { Navbar } from "./components/navbar/navbar";
-import { Footer } from "./components/footer/footer";
-import { Homepage } from "./components/homepage/homepage";
-import { Inspiration } from "./inspriration/inspiration";
-import { Article2 } from "./components/article2/article2";
-
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { Homepage } from './components/homepage/homepage';
+import { Inspiration } from './inspriration/inspiration';
+import { Article2 } from './components/article2/article2';
 
 function App() {
   return (
@@ -11,12 +10,13 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='article1' element={<Article1 />} />
-        <Route path='inspiration' element={<Inspiration />} />
-        <Route path='inspiration2' element={<Inspiration2 />} />
-        <Route path='login' element={<Login />} />
-        <Route path='article2' element={<Article2 />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="article1" element={<Article1 />} />
+        <Route path="inspiration" element={<Inspiration />} />
+        <Route path="inspiration2" element={<Inspiration2 />} />
+        <Route path="login" element={<Login />} />
+        <Route path="article2" element={<Article2 />} />
+        <Route path="/products/:slug" element={<ProductDetail />} />
       </Routes>
 
       <Footer />
