fb644b6653e383ab1ee350fa69fe124f20a17ab3
Fitria Aryani <fitriaaryani281103@gmail.com>
2024-11-19 14:07:01 +0700

    error

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import { Navbar } from "./components/navbar/navbar";
 import { Footer } from "./components/footer/footer";
 
 import { Homepage } from "./components/homepage/homepage";
-import { Inspiration } from "./inspriration/inspiration";
+import { Inspiration } from "./components/inspiration/inspiration";
 import { Article2 } from "./components/article2/article2";
+import { Article1 } from "./components/article1/article1";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Navbar />
-			
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="article1" element={<Article1 />} />
-				<Route path="inspiration" element={<Inspiration />} />
-			</Routes>
-			
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='article2' element={<Article2 />} />
+        <Route path='inspiration' element={<Inspiration />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
