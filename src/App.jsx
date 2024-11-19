7e1beada73b83b5ee264f847d6d59f5de5b648e7
dillanfdr <nurfadillahdwirahma@gmail.com>
2024-11-19 22:00:19 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { Login } from "./components/login/login";
 import { Homepage } from './components/homepage/homepage';
 import { Inspiration } from './components/inspiration/inspiration';
+import { Inspiration2 }  from "./components/inpiration2/inspiration2";
 import { Article2 } from './components/article2/article2';
 import { Article1 } from './components/article1/article1';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProductPage from './pages/Product/ProductPage';
-import { UserProvider } from './context/UserContext';
-import ProductDetail from './pages/Product/ProductDetail/ProductDetail';
+import Property from "./page/property/Property";
+import ProductDetail from "./page/ProductDetail/ProductDetail";
 
 function App() {
   return (
@@ -16,13 +17,13 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="article1" element={<Article1 />} />
+        <Route path="login" element={<Login />} />
         <Route path="inspiration" element={<Inspiration />} />
         <Route path="inspiration2" element={<Inspiration2 />} />
-        <Route path="login" element={<Login />} />
+				<Route path="article1" element={<Article1 />} />
         <Route path="article2" element={<Article2 />} />
         <Route path="/products/:slug" element={<ProductDetail />} />
-        <Route path="/roductPage" element={<ProductPage />} />
+        <Route path="property" element={<Property />} />
       </Routes>
 
       <Footer />
