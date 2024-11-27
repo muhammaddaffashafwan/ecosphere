c350e8b86ae8130601e7219541819e4de93102ef
dillanfdr <nurfadillahdwirahma@gmail.com>
2024-11-27 20:37:39 +0700

    index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,11 @@ import { getUsers } from "../controllers/Users.js";
 console.log("getUsers imported successfully:", getUsers);
 const router = express.Router();
 
+// tes web uda jalan
+router.get('/', (req, res) => {
+  res.json({status: "success"});
+});
+
 router.get('/users', getUsers);
 
 export default router;
