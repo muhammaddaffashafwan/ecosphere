6e4ac90f703376c3b87bbf6fb7d58afb9a69eb9b
thyrxz <javasantika@gmail.com>
2024-12-07 01:04:13 +0700

    responsif mobile, tab and updt web

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,20 +7,18 @@ export default function Card({ product }) {
   return (
     <Link
       to={`/products/${product.slug}`}
-      className="flex flex-col max-w-[260px] flex-wrap p-[16px] bg-[#ffffff] hover:ring-opacity-40 active:ring-5 active:ring-[#739646] rounded-[20px] shadow-xl hover:shadow-xxl transition-shadow duration-200 ease-in-out"
+      className="flex flex-col max-w-[220px] flex-wrap p-[12px] bg-[#ffffff] hover:ring-opacity-40 active:ring-5 active:ring-[#739646] rounded-[20px] shadow-xl hover:shadow-xxl transition-shadow duration-200 ease-in-out transform scale-90"
     >
-      <div className=" flex-col max-w-[250px] w-[230px]  flex-wrap p-[16px] bg-[#ffffff] rounded-[20px]">
-        <img src={product.imageUrl ?? ''} alt={product.name ?? 'No name'} className="block max-h-[300px] mb-4 object-cover" />
+      <div className="flex-col max-w-[210px] w-[200px] flex-wrap p-[12px] bg-[#ffffff] rounded-[20px]">
+        <img src={product.imageUrl ?? ''} alt={product.name ?? 'No name'} className="block max-h-[250px] mb-4 object-cover" />
         <div className="flex flex-col gap-2">
-          <div className="font-medium text-[16px] text-[#000000] text-left" style={{ textDecoration: 'none !important' }}>
-            {product.name ?? 'No Name'}
-          </div>
-          <div className="block font-medium text-[12px] text-[#3C4563]">{product.category ?? 'Uncatagorized'}</div>
-          <div className="block font-medium text-[16px] text-[#000000]">{formatToIDRCurrency(product.price) ?? 'Not for sale'}</div>
+          <div className="font-medium text-[14px] text-[#000000] text-left">{product.name ?? 'No Name'}</div>
+          <div className="block font-medium text-[10px] text-[#3C4563]">{product.category ?? 'Uncategorized'}</div>
+          <div className="block font-medium text-[14px] text-[#000000]">{formatToIDRCurrency(product.price) ?? 'Not for sale'}</div>
           <div>
             <Button
               type="button"
-              className="inline-flex items-center justify-center text-[13px] pt-2 pb-2 px-[44px] bg-[#739646] border-[#5f7f33] text-[#ffffff] hover:text-[#739646] hover:bg-[#ffffff] hover:border-[#5f7f33] active:bg-[#ffffff] hover:ring-[#739646] hover:ring-2 active:ring-2 active:border-[#5f7f33] rounded-[40px] transition-all"
+              className="inline-flex items-center justify-center text-[11px] pt-1 pb-1 px-[36px] bg-[#739646] border-[#5f7f33] text-[#ffffff] hover:text-[#739646] hover:bg-[#ffffff] hover:border-[#5f7f33] active:bg-[#ffffff] hover:ring-[#739646] hover:ring-2 active:ring-2 active:border-[#5f7f33] rounded-[40px] transition-all"
             >
               <span className="text-center">More information</span>
             </Button>
@@ -30,7 +28,3 @@ export default function Card({ product }) {
     </Link>
   );
 }
-
-Card.propTypes = {
-  product: PropTypes.object,
-};
--- src/page/property/Property.jsx
+++ src/page/property/Property.jsx
@@ -72,56 +72,50 @@ export default function Property() {
 
   return (
     <div className="bg-white min-h-screen text-black">
-    <div className="absolute top-[100px] left-[20px] sm:left-[150px] w-full sm:w-[544.1px] h-auto sm:h-[411.8px] text-left text-black font-[Optician Sans]">
-      <div className="discovery flex flex-col sm:flex-row items-center">
-        <img
-          className="absolute ml-0 mobile:w-[500px] mobile:ml-[450px] sm:ml-[605px] top-0 left-0 z-0 w-[200px] sm:w-[544px] h-auto"
-          alt="Eco Discovery"
-          src="assets/prop_illus.jpg"
-        />
-        <div className="mt-[-20px] tracking-[-0.02em] pt-[30px] leading-[20px] text-black px-4 sm:px-0">
-          <h1 className="font-bold text-[32px] sm:text-[45px] text-center sm:text-left">
-            DISCOVER ECO-FRIENDLY HOMES AND PRODUCTS
-          </h1>
-          <p className="mt-4 text-sm sm:text-base">
-            We offer a comprehensive range of eco-friendly homes and goods, coupled with cutting-edge technology. Our product range includes solar panels, home energy management systems, and smart LED lights, all designed to create a greener, more efficient home.
-          </p>
-        </div>
-      </div>
-
-      <div className="numbers mt-12 flex flex-wrap justify-center sm:justify-between text-center">
-        {/* Property Return Rate */}
-        <div className="property-return-rate flex flex-col items-center space-y-4">
-          <img className="icon h-[50px] sm:h-[80px] md:h-[80px] w-auto" alt="Property Icon" src="assets/Icon_prop.jpg" />
-          <div className="text-[12px] sm:text-sm md:text-base">
-            <p className="b text-xl md:text-[22px] text-center font-bold">7.4%</p>
-            <p className="property-return-rate1 text-black font-medium text-center">Property Return Rate</p>
+      <div className="absolute top-[100px] left-[20px] sm:left-[150px] w-full sm:w-[544.1px] h-auto sm:h-[411.8px] text-left text-black font-[Optician Sans]">
+        <div className="discovery flex flex-col sm:flex-row items-center">
+          <img className="absolute ml-0 mobile:w-[500px] mobile:ml-[450px] sm:ml-[605px] top-0 left-0 z-0 w-[200px] sm:w-[544px] h-auto" alt="Eco Discovery" src="assets/prop_illus.jpg" />
+          <div className="mt-[-20px] tracking-[-0.02em] pt-[30px] leading-[20px] text-black px-4 sm:px-0">
+            <h1 className="font-bold text-[32px] sm:text-[45px] text-justify sm:text-left">DISCOVER ECO-FRIENDLY HOMES AND PRODUCTS</h1>
+            <p className="mt-4 text-sm sm:text-base">
+              We offer a comprehensive range of eco-friendly homes and goods, coupled with cutting-edge technology. Our product range includes solar panels, home energy management systems, and smart LED lights, all designed to create a
+              greener, more efficient home.
+            </p>
           </div>
         </div>
 
-        {/* Years of Experience in Real Estate */}
-        <div className="daily-completed-transactions flex flex-col items-center space-y-4">
-          <img className="icon1 h-[50px] sm:h-[80px] md:h-[80px] w-auto" alt="Experience Icon" src="assets/Icon_estate.jpg" />
-          <div className="text-[12px] sm:text-sm md:text-base">
-            <p className="b1 text-xl md:text-[22px] text-center font-bold">10</p>
-            <p className="years-experience-in text-black font-medium text-center">Years experience in real estate</p>
+        <div className="numbers mt-12 flex flex-wrap justify-center sm:justify-between text-center">
+          {/* Property Return Rate */}
+          <div className="property-return-rate flex flex-col items-center space-y-4">
+            <img className="icon h-[50px] sm:h-[80px] md:h-[80px] w-auto" alt="Property Icon" src="assets/Icon_prop.jpg" />
+            <div className="text-[12px] sm:text-sm md:text-base">
+              <p className="b text-xl md:text-[22px] text-center font-bold">7.4%</p>
+              <p className="property-return-rate1 text-black font-medium text-center">Property Return Rate</p>
+            </div>
           </div>
-        </div>
 
-        {/* Happy Customers */}
-        <div className="property-in-sell-rent flex flex-col items-center space-y-4">
-          <img className="icon2 h-[50px] sm:h-[80px] md:h-[80px] w-auto" alt="Customer Icon" src="assets/Icon_customers.jpg" />
-          <div className="text-[12px] sm:text-sm md:text-base">
-            <p className="b2 text-xl md:text-[22px] text-center font-bold">3,856</p>
-            <p className="text-black font-medium text-center">Happy Customers</p>
+          {/* Years of Experience in Real Estate */}
+          <div className="daily-completed-transactions flex flex-col items-center space-y-4">
+            <img className="icon1 h-[50px] sm:h-[80px] md:h-[80px] w-auto" alt="Experience Icon" src="assets/Icon_estate.jpg" />
+            <div className="text-[12px] sm:text-sm md:text-base">
+              <p className="b1 text-xl md:text-[22px] text-center font-bold">10</p>
+              <p className="years-experience-in text-black font-medium text-center">Years experience in real estate</p>
+            </div>
+          </div>
+
+          {/* Happy Customers */}
+          <div className="property-in-sell-rent flex flex-col items-center space-y-4">
+            <img className="icon2 h-[50px] sm:h-[80px] md:h-[80px] w-auto" alt="Customer Icon" src="assets/Icon_customers.jpg" />
+            <div className="text-[12px] sm:text-sm md:text-base">
+              <p className="b2 text-xl md:text-[22px] text-center font-bold">3,856</p>
+              <p className="text-black font-medium text-center">Happy Customers</p>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-  
 
       {/* Partners Section */}
-      <div className=" absolute top-[650px] left-[calc(50%-670px)] w-[1340px] flex flex-col items-center gap-[30px]">
+      <div className=" relative top-[760px] left-[calc(50%-670px)] w-[1340px] flex flex-col items-center gap-[30px]">
         <h1 className=" font-bold  text-[30px] relative flex flex-col  items-center gap-[35px]">OUR PARTNERS</h1>
         <div id="partnersCarousel" className="carousel slide relative">
           <div className="carousel-inner">
@@ -132,11 +126,11 @@ export default function Property() {
                 <img className="partner-logo w-auto h-16 " src="assets/xiaomi.jpg" alt="Partner 3" />
                 <img className="partner-logo w-auto h-16 " src="assets/patner_4.jpg" alt="Partner 4" />
                 <img className="partner-logo w-auto h-16 " src="assets/patner_2.jpg" alt="Partner 2" />
-                <img className="partner-logo w-auto h-16 " src="assets/patner_3.jpg" alt="Partner 3" />
               </div>
             </div>
             <div className="carousel-item active flex justify-center space-x-8">
               <div className="flex justify-center gap-[15px]">
+                <img className="partner-logo w-auto h-16 " src="assets/patner_3.jpg" alt="Partner 3" />
                 <img className="partner-logo w-auto h-16 " src="assets/ciputra.jpg" alt="Partner 2" />
                 <img className="partner-logo w-auto h-16 " src="assets/99co.jpg" alt="Partner 3" />
                 <img className="partner-logo w-auto h-16 " src="assets/agungsaha.jpg" alt="Partner 4" />
@@ -150,17 +144,16 @@ export default function Property() {
       <div className="px-4 md:px-16 lg:px-24 pt-[900px] pb-[4px] gap-4 mt-4 flex-wrap">
         <h1 className="text-center text-[24px] md:text-[30px] font-bold">PROPERTY RECOMMENDATION</h1>
         <p className="mt-2 md:mt-4 mb-2 md:mb-4 text-center text-sm md:text-base">We&apos;re all about smart innovation for a greener life</p>
-        <h3 className="lg:ml-[100px] px-4 md:px-16 lg:px-24 font-medium text-black">Filter</h3>
+        <h3 className="lg:ml-[100px] px-4 md:px-16 lg:px-24 font-medium text-black mt-6">Filter</h3>
         <div className="lg:ml-[100px] px-4 md:px-16 lg:px-24 flex gap-2 flex-wrap">
           <RadioButton options={radioButtonOpts.current} defaultValue={'all'} onChange={handleCategoryChange} />
         </div>
       </div>
       <section className="mb-[125px] container px-4 md:px-16 lg:px-24 py-4">
-        <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-[80px]">
           <CardList products={products} />
         </main>
       </section>
     </div>
-    
   );
-} 
+}
